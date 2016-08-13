e6a0f3e90754d97c947f6882494c787692dba6b7
lmns1314HuHu <851549562@qq.com>
2016-08-13 18:02:22 +0800

    daily_commit

--- 图论/Gym_100735H_图论_二分图最大匹配.cpp
+++ 图论/Gym_100735H_图论_二分图最大匹配.cpp
@@ -8,6 +8,7 @@ char str[30];
 char _cube[110][6];
 //顶点编号从0开始的
 #define MAXN 110
+
 int uN,vN;//u,v数目
 int g[MAXN][MAXN];
 int linker[MAXN];
@@ -15,7 +16,7 @@ bool used[MAXN];
 
 bool dfs(int u)//从左边开始找增广路径
 {
-    for(int v=0; v<vN; v++){//这个顶点编号从0开始，若要从1开始需要修改
+    for(int v=0; v<vN; v++){//这个顶点编号从0开始，若要从1开始，则需要修改
         if(g[u][v]&&!used[v]){
             used[v]=true;
             if(linker[v]==-1||dfs(linker[v])){//找增广路，反向
